382dfba7dace82d632109c19c3e76f11edff47ab
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-06-24 03:01:47 +0100

    i

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default async function Hero() {
   return (
-    <section className="flex flex-col items-center justify-center p-4 space-y-8 pb-[48px] md:py-32  md:flex-row md:space-x-16 md:space-y-0 max-w-[1200px] mx-auto">
+    <section className="flex flex-col items-center justify-center p-4 space-y-12 pb-[48px] md:py-32  md:flex-row md:space-x-16 md:space-y-0 max-w-[1200px] mx-auto">
       <Image
         //src="/placeholder.svg"
         //src="/cordovia-2.jpeg"
@@ -17,8 +17,8 @@ export default async function Hero() {
         alt="Hero"
         className="mx-auto h-[300px] md:h-[400px] w-full overflow-hidden rounded-xl object-cover   blur"
       />
-      <div className="flex flex-col gap-6">
-        <section className="flex flex-col gap-2 ">
+      <div className="flex flex-col gap-8">
+        <section className="flex flex-col gap-4 ">
           <h1 className="text-3xl sm:text-5xl xl:text-6xl font-bold tracking-tighter">
             Elevate Your Brand with{" "}
             <span className="text-[#DA0785] text-4xl sm:text-5xl xl:text-6xl ">
@@ -32,14 +32,14 @@ export default async function Hero() {
           </p>
         </section>
 
-        <section className="flex flex-row gap-2 ">
-          <Link href="/services" className="flex w-full">
-            <button className="bg-black border border-black text-sm font-medium rounded-md  text-white p-2 text-center px-4 w-full tracking-tighter max-w-[200px]">
+        <section className="flex flex-row gap-2 w-full">
+          <Link href="/services" className="flex w-full max-w-[200px]">
+            <button className="bg-black border border-black text-sm font-medium rounded-md  text-white p-2 text-center px-4 w-full tracking-tighter ">
               Our Services
             </button>
           </Link>
-          <Link href="/contact" className="flex w-full">
-            <button className="bg-white border border-gray-300 text-sm font-medium rounded-md text-black p-2 text-center px-4 w-full tracking-tighter max-w-[200px]">
+          <Link href="/contact" className="flex w-full max-w-[200px]">
+            <button className="bg-white border border-gray-300 text-sm font-medium rounded-md text-black p-2 text-center px-4 w-full tracking-tighter ">
               Contact Us
             </button>
           </Link>
