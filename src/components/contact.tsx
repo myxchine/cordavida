aa834741a31390005c360825151800b6e07e7b1f
Michael Dos Santos <michael.dozzantoz@gmail.com>
2024-06-24 02:26:44 +0100

    i

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -39,14 +39,17 @@ export default function Contact() {
   };
 
   return (
-    <section id="contact" className="w-full py-12 md:py-24 lg:py-32 bg-black/5">
+    <section
+      id="contact"
+      className="w-full py-12 md:py-24 lg:py-32 bg-black/5 flex flex-col items-center justify-center"
+    >
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
               Get in Touch
             </h2>
-            <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+            <p className="max-w-[500px] text-black/60 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
               Have a project in mind? Contact us to discuss how Cordavida can
               help bring your vision to life.
             </p>
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col items-center justify-center p-4 space-y-8 pb-[48px]">
+    <section className="flex flex-col items-center justify-center p-4 space-y-8 pb-[48px] md:py-32  md:flex-row md:space-x-16 md:space-y-0 max-w-[1200px] mx-auto">
       <Image
         //src="/placeholder.svg"
         src="/cordovia-2.jpeg"
@@ -10,13 +10,16 @@ export default function Hero() {
         height={400}
         priority
         alt="Hero"
-        className="mx-auto h-[300px] w-full overflow-hidden rounded-xl object-cover sm:w-full lg:order-last blur"
+        className="mx-auto h-[300px] md:h-[400px] w-full overflow-hidden rounded-xl object-cover   blur"
       />
       <div className="flex flex-col gap-6">
         <section className="flex flex-col gap-2 ">
           <h1 className="text-3xl sm:text-5xl xl:text-6xl font-bold tracking-tighter">
             Elevate Your Brand with{" "}
-            <span className="text-[#DA0785]">Cordavida</span>.
+            <span className="text-[#DA0785] text-4xl sm:text-5xl xl:text-6xl ">
+              Cordavida
+            </span>
+            .
           </h1>
           <p className="text text-black/60 max-w-[600px] md:text-xl">
             Cordavida is a design and large format printing company dedicated to
